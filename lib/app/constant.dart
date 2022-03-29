3546f51a1ec21e922198be849e7c88f4c2e1c8a3
Mohamed Abdelwahab <maabdel22@gmail.com>
2022-03-29 11:38:53 +0200

    update dio factory with share Preferences

--- lib/app/constant.dart
+++ lib/app/constant.dart
@@ -1,5 +1,8 @@
-class Constant {
+class Constants {
   static const String baseUrl = "http://mohamedabdelwahab.mocklab.io";
   static const EMPTY = "";
   static const ZERO = 0;
+  static const String token = "SEND TOKEN HERE";
+  static const int zero = 0;
+  static const int apiTimeOut = 60000;
 }
